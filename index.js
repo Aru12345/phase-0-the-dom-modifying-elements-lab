9275aa810fa05d5d9207946ed121823d25e33a7f
Arundhati <arundhatiraicar12@gmail.com>
2021-08-06 22:59:24 +0530

    commit tree 8db8b1d9639d1a60a12ef4cd09b19098b7f0a685

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+
+//document.querySelector("main#main").remove();
+document.getElementById("main").remove();
+const newHeader =document.createElement('h1');
+newHeader.id ="victory";
+newHeader.innerHTML="Arundhati is the champion"
